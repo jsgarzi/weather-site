28c1244d09321ac1d7d5e985ec25ac7bc5e35fff
jsgarzi <jsgarzi@fortitudedevelopment.com>
2020-11-25 14:39:07 -0500

    logging stuff~

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ const api = '773470a2496008d31462265d05fc9252';
 
 let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + api; 
 
+let currentTime = moment().format("MMM, Do, YYYY");
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response){
     console.log(response)
+    //city
+    console.log(response.name)
+    
+    //current weather
+    console.log(response.main.temp)
+    //current cond
+    console.log(response.weather[0].main)
+    //humidity %
+    console.log(response.main.humidity)
+    //wind speed mph 
+    console.log(response.wind.speed)  
+    //uv
+
 });
+
+console.log("("+currentTime+")");

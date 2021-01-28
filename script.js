cfdfc4e7e039642b38828c3298f09f3c5f33be92
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-01-28 14:49:45 -0500

    changed jquery version (works now)

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-let city = "boston";
 
-const api = '773470a2496008d31462265d05fc9252';
-
-let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + api; 
-
-
-$.ajax({
-    url: queryURL
-}).then(function (response){
-    console.log(response)
-    //city
-    let city = response.name
-    const cityEl = $("#current-WTI")
-    cityEl.append(city + " ")
-    console.log(city)
-    //date
-    let currentTime = moment().format("MMM, Do, YYYY");
-    const currTimeEl = $("#current-WTI");
-    currTimeEl.append("("+currentTime+") ");
-    console.log("("+currentTime+")");
-    //current weather
-    let currtWeath = response.main.temp
-    console.log("Temperature: " + currtWeath + " °F")
-    //current cond
-    let condition = response.weather[0].main
-    console.log(condition)
-    //humidity %
-    let humidity = response.main.humidity
-    console.log(humidity + "% Humidity")
-    //wind speed mph 
-    let windSpeed = response.wind.speed
-    console.log(windSpeed +" mph")  
-    //uv??
+$("#city-search").on("click", function () {
+    var city = $("#location").val();
+    const api = '773470a2496008d31462265d05fc9252';
+    let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + api;
     
-});
+    $.ajax({
+        type: "GET",
+        url: queryURL
+    }).then(function (response) {
+        console.log(response)
+        //city
+        let city = response.name
+        const cityEl = $("#current-WTI")
+        cityEl.append(city + " ")
+        //date
+        let currentTime = moment().format("MMM, Do, YYYY");
+        const currTimeEl = $("#current-WTI");
+        currTimeEl.append("(" + currentTime + ") ");
+        console.log("(" + currentTime + ")");
+        //current weather
+        let currtWeath = response.main.temp
+        console.log("Temperature: " + currtWeath + " °F")
+        //current cond
+        let condition = response.weather[0].main
+        console.log(condition)
+        //humidity %
+        let humidity = response.main.humidity
+        console.log(humidity + "% Humidity")
+        //wind speed mph 
+        let windSpeed = response.wind.speed
+        console.log(windSpeed + " mph")
+        //uv??
+    
+    });
+})
+
+
+
+
+
+
+
 
 // let currentTime = moment().format("MMM, Do, YYYY");
 // const currTimeEl = $("#current-WTI");
 // currTimeEl.append("("+currentTime+") ");
 // console.log("("+currentTime+")");
+
+// need to call weather for next 5 day 
+
+
+
